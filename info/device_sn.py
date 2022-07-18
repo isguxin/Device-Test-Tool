338b9b81abfad2ffaaddab425d57d8cd9ced3e12
guxin <g_xin@outlook.com>
2022-07-18 23:20:50 +0800

    update

--- info/device_sn.py
+++ info/device_sn.py
@@ -8,6 +8,23 @@ class DeviceSN(MainUI):
 
         self.get_device_sn_button["command"] = self.get_devices_sn_list
 
+    def get_devices_sn_list(self):
+        """
+        获取设备的 SN 号
+
+        :return: None
+        """
+        devices_sn_list = ADBTool.get_devices_sn_list()
+        if len(devices_sn_list) == 0:
+            self.get_device_sn_combobox["values"] = ["No devices found"]
+            self.log("未连接设备")
+        else:
+            self.get_device_sn_combobox["values"] = devices_sn_list
+            self.get_device_sn_combobox.current(0)
+            self.log("设备SN列表：{}".format(devices_sn_list))
+            self.log("默认选择SN：{}".format(self.get_device_sn_combobox.get()))
+            self.get_device_sn_combobox.bind('<<ComboboxSelected>>', self.ensure)
+
     def ensure(self, event):
         """
         日志展示选中的设备 SN 号
@@ -17,21 +34,3 @@ class DeviceSN(MainUI):
         """
         sn = self.get_device_sn_combobox.get()
         self.log("已选择SN：{}".format(sn))
-
-    def get_devices_sn_list(self):
-        """
-        获取设备的 SN 号
-
-        :return: None
-        """
-        # devices_sn_list = ADBTool.get_devices_sn_list()
-        devices_sn_list = ["123132", "123123123123123"]
-        if len(devices_sn_list) == 0:
-            self.get_device_sn_combobox["values"] = ["No devices found"]
-            self.log("未连接设备")
-        else:
-            self.get_device_sn_combobox["values"] = devices_sn_list
-            self.get_device_sn_combobox.current(0)
-            self.log("设备SN列表：{}".format(devices_sn_list))
-            self.log("已选择SN：{}".format(self.get_device_sn_combobox.get()))
-            self.get_device_sn_combobox.bind('<<ComboboxSelected>>', self.ensure)
