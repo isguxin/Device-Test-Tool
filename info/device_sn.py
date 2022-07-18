8b4d32a5fd45763491e6fdc4ffe2cae7548af03a
guxin <g_xin@outlook.com>
2022-07-18 22:51:37 +0800

    update

--- info/device_sn.py
+++ info/device_sn.py
@@ -1,3 +1,4 @@
+from common.adb_tool import ADBTool
 from gui.main_gui import MainUI
 
 
@@ -7,13 +8,30 @@ class DeviceSN(MainUI):
 
         self.get_device_sn_button["command"] = self.get_devices_sn_list
 
-    def get_devices_sn_list(self):
-        devices_sn_list = ["127:0.0.1:7555", "123123123"]
-        self.get_device_sn_combobox["values"] = devices_sn_list
-        self.get_device_sn_combobox.current(0)
-        self.get_device_sn_combobox.bind("<<ComboboxSelected>>", self.ensure)
-
     def ensure(self, event):
-        self.log(self.get_device_sn_combobox.get())
+        """
+        日志展示选中的设备 SN 号
 
+        :param event: 动作
+        :return: None
+        """
+        sn = self.get_device_sn_combobox.get()
+        self.log("已选择SN：{}".format(sn))
 
+    def get_devices_sn_list(self):
+        """
+        获取设备的 SN 号
+
+        :return: None
+        """
+        # devices_sn_list = ADBTool.get_devices_sn_list()
+        devices_sn_list = ["123132", "123123123123123"]
+        if len(devices_sn_list) == 0:
+            self.get_device_sn_combobox["values"] = ["No devices found"]
+            self.log("未连接设备")
+        else:
+            self.get_device_sn_combobox["values"] = devices_sn_list
+            self.get_device_sn_combobox.current(0)
+            self.log("设备SN列表：{}".format(devices_sn_list))
+            self.log("已选择SN：{}".format(self.get_device_sn_combobox.get()))
+            self.get_device_sn_combobox.bind('<<ComboboxSelected>>', self.ensure)
